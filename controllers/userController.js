4d7f960494774b878f21ba24f59c4f6ce8e5506f
Rafhael Prates <fellepy960@gmail.com>
2026-06-03 23:51:10 -0300

    feat: adiciona hash de senha com bcrypt e e-mail unico
    - Hash da senha no cadastro com bcryptjs e verificacao no login com compare
    - Verifica e-mail ja cadastrado antes de criar o usuario (findOne)
    - Adiciona constraint unique no campo email (model + sync alter)
    - Corrige bug de "headers already sent" no register_post

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,44 +1,66 @@
+
 import { User } from '../models/user.js';
+import bcryptjs  from 'bcryptjs'
+
 
 const register_get = (req,res)=>{
     res.render('user/register')
 }
-const register_post = (req,res)=>{
+const  register_post = async (req,res)=>{
 
-    const {nome,email,senha} = req.body
-    
-    User.create({
-        nome: nome,
-        email: email,
-        senha: senha
-    }).then(()=>{
-        res.sendStatus(201)
+   try{
 
-    }).catch((err)=>{
-        res.status(400).json({'Erro:':err})
-    })
+    const {nome,email} = req.body
+
+    const email_disponivel = await User.findOne({where:{'email':email}})
+
+    if(email_disponivel){
+        return res.status(409).json({erro:'Email indísponivel'})        
+
+    }else{ 
+        
+        const senha = await bcryptjs.hash(req.body.senha,10)
+
+        await User.create({
+       
+            nome: nome,
+            email: email,
+            senha: senha
+
+        })  
+            return res.sendStatus(201)
+    }
     
+   }catch (err){
+        return res.status(400).json({erro:'ERRO ao criar usuario'})
+   }  
 }
 const login_get = (req,res)=>{
     res.render('user/login')
 }
-const login_post = (req,res)=>{
+const login_post = async (req,res)=>{
 
     const {email,senha} = req.body
 
-    User.findOne({where:{'email':email}}).then((user)=>{
+    try{
 
-        if(user){
-
-            if(senha === user.senha){
-                return res.sendStatus(200)
-            }else {
-                return res.status(401).json({'Erro:':'Credenciais invalidas'})
-            }
-        }else{
-            return res.status(401).json({'Erro:': 'Credenciais invalidas'})
+        const user = await User.findOne({where: { 'email':email } })
+        
+        if(!user){
+            return res.status(401).json({erro:'Credenciais inválidas'})
         }
-    })
+
+        const match = await bcryptjs.compare(senha,user.senha)
+
+        if(match){
+            return res.sendStatus(200)
+        }else{
+            return res.status(401).json({erro:'Credenciais inválidas'})
+        }
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({erro:'Erro interno'})
+    }
 }
 
 export default{
--- models/user.js
+++ models/user.js
@@ -3,13 +3,16 @@ import { Sequelize } from "sequelize";
 import { db } from "./db.js";
 
 
+
+
  const User = db.sequelize.define('User',{
 
     nome: { type: db.Sequelize.STRING },
-    email:{ type: db.Sequelize.STRING },
+    email:{ type: db.Sequelize.STRING, unique:true },
     senha: { type: db.Sequelize.STRING }
 
  })
 
+//  User.sync({alter:true})
 
  export { User }
